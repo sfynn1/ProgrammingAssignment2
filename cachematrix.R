6468a2f8c10980964996cc61e02d157f14459eb1
sfynn1 <sean.fynn@gmail.com>
2015-11-15 21:24:50 -0700

    rough

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+
+
+## --------- TESTING FUNCTION -----------##
+# generate a random matrix
+testm <- matrix(sample.int(100, 5*5, TRUE), 5, 5)
+
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+# Take matrix arguemnt and load to global memory
+  m1 <<- x
+  m1inverse <<- solve(x)
 }
 
 
@@ -12,4 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(x==m1){
+     output <- m1inverse
+  }  else
+  { output <- solve(x)}
+  return(output)
 }

6bacbce8d38a070abab48e5869799163a3bca215
sfynn1 <sean.fynn@gmail.com>
2015-11-16 22:29:00 -0700

    add in singular check revised 1

--- cachematrix.R
+++ cachematrix.R
@@ -5,8 +5,8 @@
 
 ## --------- TESTING FUNCTION -----------##
 # generate a random matrix
-testm <- matrix(sample.int(100, 5*5, TRUE), 5, 5)
-
+testm <- matrix(sample.int(100, 5*5, TRUE), 5, 5)  #good
+errM <- matrix( c(2,2,3,6,6,9,1,4,8) ,nrow = 3,ncol = 3) #explicit singular matrix
 
 ## Write a short comment describing this function
 
@@ -37,40 +37,30 @@ makeCacheMatrix <- function(original = matrix()) {
 cacheSolve <- function(origMat, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-#   m <- x$getmean()
-#   if(!is.null(m)) {
-#     message("getting cached data")
-#     return(m)
-#   }
-#   data <- x$get()
-#   m <- mean(data, ...)
-#   x$setmean(m)
-#   m
-
   invMat <- test$getInverse()
-  if (!is.null(invMat)) {
-    message("getting cached data")
-    return(invMat)
-  } else {
-  originalMatrix <- test$get()
-  invMat <- solve(originalMatrix)
-  test$setInverse(invMat)
+  if ( !is.null(invMat) ) {
 
-  return(invMat)
+        message("getting cached data")
+
+    return(invMat)
   }
+    else
+         {
+          originalMatrix <- test$get()
+
+             # Test to ensure no errors, such as non-invertable matrix
+              errorString <- NULL
+              errorString <- tryCatch(solve(originalMatrix) %*% originalMatrix, error = function(e) e)  #capture any error
+              if ( any(class(errorString) == "error") )   #test for error and exit
+                     stop ("Singular Matrix - Please try a different matrix!")
+
+              invMat <- solve(originalMatrix)
+              test$setInverse(invMat)
+
+          return(invMat)
+         }
 }
 
-
-## References USED:
+## --------------  References USED: ---------------- ##
 #  https://cran.r-project.org/doc/manuals/R-intro.html#DOCF23  Open.Account example
-
-
 #  http://stackoverflow.com/questions/24961983/how-to-check-if-a-matrix-has-an-inverse-in-the-r-language
-errM <- matrix( c(2,2,3,6,6,9,1,4,8) ,nrow = 3,ncol = 3) #explicit singular matrix
-
-out <- tryCatch(solve(errM) %*% errM, error = function(e) e)  #capture any error
-any(class(out) == "error")  #test for error
-
-
-
-
